b8a313e9c432fcbb92214a6db9b5b6ce7f58c944
SimoneCerri <simone.cerri1994@gmail.com>
2024-09-09 16:43:32 +0200

    simple layout

--- src/components/Pokedex.vue
+++ src/components/Pokedex.vue
@@ -3,11 +3,36 @@
 </script>
 
 <template>
-  <div class="container">
-    <div class="row">
-      <div class="col-6"></div>
+  <div class="container pt-5">
+    <div class="row justify-content-center">
+      <div class="col-6 d-flex flex-column align-items-center">
+        <div class="poke-search">
+          <input class="search-bar mt-3" type="text" placeholder="Who's that pokemon ?" v-model="search">
+          <button class="search-button">
+            Search
+          </button>
+        </div>
+        <div class="poke-img mt-3">
+          <img src="" alt="">
+        </div>
+        <div class="poke-stats mt-5">
+        </div>
+      </div>
       <!-- /.col-6 -->
-      <div class="col-6"></div>
+      <div class="col-6 d-flex align-items-center flex-column">
+        <button class="poke-add mt-3">
+          Catch it !
+        </button>
+        <div class="poke-captured mt-3">
+          <h1 class="text-center">
+            My Pokemon :
+          </h1>
+          <ul class="list-unstyled">
+            <li>
+            </li>
+          </ul>
+        </div>
+      </div>
       <!-- /.col-6 -->
     </div>
     <!-- /.row -->
@@ -18,7 +43,26 @@
 <style scoped>
 .col-6 {
   background-color: red;
+  height: 700px;
+  width: 500px;
+  border: 1px solid black;
+}
+
+.poke-img {
+  background-color: white;
   height: 200px;
-  width: 200px;
+  width: 80%;
+}
+
+.poke-stats {
+  background-color: green;
+  height: 350px;
+  width: 80%;
+}
+
+.poke-captured {
+  background-color: white;
+  height: 600px;
+  width: 80%;
 }
 </style>
