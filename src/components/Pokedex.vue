6107dfcd64bd2a634e5560b7eb00d6b18ba44979
SimoneCerri <simone.cerri1994@gmail.com>
2024-09-09 16:15:56 +0200

    add bootstrap, i will work on design for last

--- src/components/Pokedex.vue
+++ src/components/Pokedex.vue
@@ -3,7 +3,22 @@
 </script>
 
 <template>
-
+  <div class="container">
+    <div class="row">
+      <div class="col-6"></div>
+      <!-- /.col-6 -->
+      <div class="col-6"></div>
+      <!-- /.col-6 -->
+    </div>
+    <!-- /.row -->
+  </div>
+  <!-- /.container -->
 </template>
 
-<style scoped></style>
+<style scoped>
+.col-6 {
+  background-color: red;
+  height: 200px;
+  width: 200px;
+}
+</style>
